f94cab4588a27b12ee3da163c32e623610555120
ilrogers42 <ilrogers42@tntech.edu>
2024-10-29 15:53:39 -0500

    Driver file update

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,7 @@
-#include<fstream>
-#include<iostream>
-#include<string>
-#include<sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
 #include "County.h"
 #include "List.h"
 
@@ -9,8 +9,11 @@ using namespace std;
 
 int main()
 {
+    ifstream file;
+	stringstream temp;
 	string line, county, state;
-	int index, pop;
+	int index = 0, pop;
+    file.open("counties_list.csv", ios::in);
 
 	List<County*> countyList;
 
@@ -24,7 +27,9 @@ int main()
         cout << "\nWhat Would you like to do?";
         cout << "\n1.  Merge Sort";
         cout << "\n2.  Selection Sort";
-        cout << "\n3.  End Program";
+        cout << "\n3.  Add a new county";
+        cout << "\n4.  Print all counties";
+        cout << "\n5.  End Program";
         cout << "\nCHOOSE 1-2:  ";
 
         cin >> choice;
@@ -37,6 +42,30 @@ int main()
                 countyList.selectionSort();
                 break;
             case 3:
+
+                if(file.good()){
+                        index++;
+
+                        cin.ignore();
+                        cout << "\nPlease enter the name of your county:\t\t";
+                        getline(cin, county);
+
+                        cout << "\nPlease enter the state in which it's located:\t";
+                        getline(cin, state);
+
+                        cout << "\nPlease enter the population of your county:\t";
+                        cin >> pop;
+
+                        newCounty = new County(index, county, state, pop);
+                        countyList.append(newCounty);
+                }
+
+                file.close();
+                break;
+            case 4:
+                countyList.print();
+                break;
+            case 5:
                 cout << "Closing program...";
                 break;
             default:
@@ -44,6 +73,5 @@ int main()
         }
     }while(choice != 3);
 
-	cin.get();
 	return 0;
 }
